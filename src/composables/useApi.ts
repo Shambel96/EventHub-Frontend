0e65d9ebda1b2406327bd02a5db6d212e136a03b
Shambel Dechu <shambeldechu@gmail.com>
2026-04-07 17:02:24 +0300

    modify: change the backend port

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -17,7 +17,7 @@ export const useApi = () => {
       onResponseError({ response }) {
         if (response.status === 401) {
           console.error('Unauthorized access - potential token expiration');
-          // authStore.logout(); // Optional: auto-logout on 401
+          authStore.logout();
         }
       }
     });
